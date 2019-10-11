ecf786991fbfdc2792dd3e8eefa8a01b8f9eeecf
Vterebenin <niceserver11@mail.ru>
2019-10-11 22:41:46 +0300

    initial front-end

---
+++ front/src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home">
+    There will be my recent thoughts and activity
+    <p>also my status and like recent picture from instagram</p>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'home',
+}
+</script>
---
+++ front/src/views/Projects.vue
@@ -0,0 +1,10 @@
+<template>
+   <div>
+       There will be the list of my projects
+   </div>
+</template>
+<script>
+   export default {
+     name: 'Projects'
+   }
+</script>
