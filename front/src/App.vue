2c04aae103a592a6d9cec755859ce9a59739bfaf
Vterebenin <niceserver11@mail.ru>
2019-10-12 15:57:31 +0300

    include octokit

--- front/src/App.vue
+++ front/src/App.vue
@@ -9,22 +9,42 @@
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+  import github from 'octonode'
+  export default {
+    name: "App",
+    data() {
+      return {
+        api: process.env.VUE_APP_GITHUB_API
+      }
+    },
+    async mounted() {
+      const client = github.client()
+      const user = await client.get('/users/vterebenin', {}, function (err, status, body, headers) {
+        console.log(body)
+      });
+
+
     }
   }
-}
+</script>
+
+<style lang="scss">
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
+  #nav {
+    padding: 30px;
+    a {
+      font-weight: bold;
+      color: #2c3e50;
+      &.router-link-exact-active {
+        color: #42b983;
+      }
+    }
+  }
 </style>
